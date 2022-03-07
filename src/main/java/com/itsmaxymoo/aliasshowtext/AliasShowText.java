a7fe15502556a7d145dbcb66328d9c92c97ba112
itsmaxymoo <max.loiacono@protonmail.com>
2022-03-07 17:40:38 -0500

    Plugin setup

---
+++ src/main/java/com/itsmaxymoo/aliasshowtext/AliasShowText.java
@@ -0,0 +1,17 @@
+package com.itsmaxymoo.aliasshowtext;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class AliasShowText extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
